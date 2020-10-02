0e1f8048491315d32a4bd64e4acdf9b017f22b7c
Dolev Pearl <19dolevp@gmail.com>
2020-10-03 00:24:36 +0300

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./app.css"
+
+export default function App() {
+  return (
+    <div className="container">
+      <header className="app-header">
+        <h2 className="title">
+          Dashboard
+        </h2>
+      </header>
+    </div>
+  );
+}
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+import * as manifest from "../../public/manifest.json"
+
+export const theme = {
+    colors: {
+        background: manifest.background_color
+    }
+}
