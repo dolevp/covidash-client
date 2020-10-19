5be6a456b65c07c9ab00d29ebc31c27f63d0ad29
Dolev Pearl <19dolevp@gmail.com>
2020-10-19 20:15:50 +0300

    Environment variables

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  API_ROOT: process.env.API_ROOT || 'http://localhost:5000',
+  API_ROOT: process.env.REACT_APP_API_URL || 'http://localhost:5000',
 }
 
 export default constants
